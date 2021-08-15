2ecf523ec037292f986776a1f241c75a8b8bc70c
Kaalleen <reni@allenka.de>
2021-08-15 18:36:48 +0200

    generate_stitch_plan

--- lib/stitch_plan/__init__.py
+++ lib/stitch_plan/__init__.py
@@ -3,6 +3,7 @@
 # Copyright (c) 2010 Authors
 # Licensed under the GNU GPL version 3.0 or later.  See the file LICENSE for details.
 
-from .stitch_plan import patches_to_stitch_plan, StitchPlan, ColorBlock
-from .stitch import Stitch
+from .generate_stitch_plan import generate_stitch_plan
 from .read_file import stitch_plan_from_file
+from .stitch import Stitch
+from .stitch_plan import ColorBlock, StitchPlan, patches_to_stitch_plan
